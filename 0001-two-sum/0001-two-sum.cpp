13b7a9fb5e4ccb132b3c68ca390d6b990c5f5e87
Adamya Kumar <101924838+Adamya-Kumar@users.noreply.github.com>
2024-09-09 12:05:57 +0000

    update two sum

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,3 +1,5 @@
+#include<bits/stdc++.h>
+using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
@@ -13,3 +15,13 @@ public:
         return {-1, -1};
     }
 };
+int main(){
+    Solution obj;
+    vector<int> nums;
+    int target=3;
+    vector<int> ans=obj.twoSum(nums,target);
+    for(auto it:ans){
+        cout<<it<<" ";
+    }
+    return 0;
+}
