7093dfbe9180af06e1576f3eb7badf2b36c7e80d
Adamya Kumar <101924838+Adamya-Kumar@users.noreply.github.com>
2025-07-17 04:23:30 +0530

    Time: 3 ms (70.71%), Space: 14.9 MB (36.83%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,27 +1,14 @@
-#include<bits/stdc++.h>
-using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        map<int, int> mpp;
-        for (int i = 0; i < nums.size(); i++) {
-            int a = nums[i];
-            int val = target - a;
-            if (mpp.find(val) != mpp.end()) {
-                return {mpp[val], i};
-            }
-            mpp[a] = i;
+        map<int,int> mpp;
+        for(int i=0;i<nums.size();i++){
+          int rem=target-nums[i];
+          if(mpp.find(rem)!=mpp.end()){
+            return {mpp[rem],i};
+          }
+          mpp[nums[i]]=i;
         }
-        return {-1, -1};
+      return {-1,-1};
     }
 };
-int main(){
-    Solution obj;
-    vector<int> nums;
-    int target=3;
-    vector<int> ans=obj.twoSum(nums,target);
-    for(auto it:ans){
-        cout<<it<<" ";
-    }
-    return 0;
-}
