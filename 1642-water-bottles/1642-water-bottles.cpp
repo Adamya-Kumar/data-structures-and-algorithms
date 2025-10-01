fb4c662a6595a72ef66add3ce9248aaca6a897fc
Adamya Kumar <101924838+Adamya-Kumar@users.noreply.github.com>
2025-10-02 00:14:17 +0530

    Time: 0 ms (100%), Space: 7.9 MB (45.09%) - LeetHub

---
+++ 1642-water-bottles/1642-water-bottles.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        int res=0;
+        int empty=0;
+        int full=0;
+        while(numBottles > 0){
+           res+=numBottles;
+           empty+=numBottles;
+           numBottles=empty/numExchange;
+          empty=empty % numExchange;
+        }
+        return res;
+    }
+};
