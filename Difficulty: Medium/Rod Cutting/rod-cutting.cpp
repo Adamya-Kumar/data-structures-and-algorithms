35042331e862f994725b4e948bc7c24384adebe0
Adamya Kumar <101924838+Adamya-Kumar@users.noreply.github.com>
2025-10-03 14:30:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Rod Cutting/rod-cutting.cpp
@@ -0,0 +1,35 @@
+// User function Template for C++
+
+class Solution {
+  public:
+  
+  int rodCuttingUnboundedKnapsack(vector<int>& price,vector<int>& length,int n){
+      long long t[n+1][n+1];
+      for(int i=0;i<n+1;i++){
+          for(int j=0;j<length.size()+1;j++){
+              if(i==0 || j == 0){
+                  t[i][j]=0;
+              }
+          }
+      }
+      
+      for(int i=1;i<n+1;i++){
+          for(int j=1;j<length.size()+1;j++){
+              if(length[i-1]<=j){
+                  t[i][j] = max(price[i-1]+t[i][j-length[i-1]],t[i-1][j]);
+              }else{
+                  t[i][j] = t[i-1][j];
+              }
+          }
+      }
+      return t[n][length.size()];
+  }
+    int cutRod(vector<int> &price) {
+        vector<int> length(price.size());
+        for(int i=0;i<price.size();i++){
+            length[i]=i+1;
+        }
+       return rodCuttingUnboundedKnapsack(price,length,price.size());
+   
+    }
+};
