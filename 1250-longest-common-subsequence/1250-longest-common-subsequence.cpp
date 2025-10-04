ca5896a2fb786c0ecdf27f698c81292360a87d92
Adamya Kumar <101924838+Adamya-Kumar@users.noreply.github.com>
2025-10-04 20:45:55 +0530

    Time: 15 ms (91.66%), Space: 12.7 MB (87.69%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -1,21 +1,35 @@
 class Solution {
 public:
-      int t[10001][1001];
-      int lcs(string& x,string& y,int n,int m){
-        if(n == 0 || m == 0){
-          return 0;
+      int LCS(string& x, string& y, int n, int m)
+{
+    int t[n + 1][m + 1];
+    for (int i = 0; i < n + 1; i++)
+    {
+        for (int j = 0; j < m + 1; j++)
+        {
+            if (i == 0 || j == 0)
+                t[i][j] = 0;
         }
-        if(t[n][m]!= -1){
-          return t[n][m];
+    }
+    for (int i = 1; i < n + 1; i++)
+    {
+        for (int j = 1; j < m + 1; j++)
+        {
+            if (x[i - 1] == y[j - 1])
+            {
+                t[i][j] = 1 + t[i - 1][j - 1];
+            }
+            else
+            {
+                t[i][j] = max(t[i][j - 1], t[i - 1][j]);
+            }
         }
-        if(x[n-1] == y[m-1]){
-          return t[n][m] = 1+lcs(x,y,n-1,m-1);
-        }else{
-          return t[n][m]=max(lcs(x,y,n-1,m),lcs(x,y,n,m-1));
-        }
-      }
+    }
+    return t[n][m];
+}
+
     int longestCommonSubsequence(string text1, string text2) {
-      memset(t,-1,sizeof(t));
-        return lcs(text1,text2,text1.size(),text2.size());
+      
+        return LCS(text1,text2,text1.size(),text2.size());
     }
 };
