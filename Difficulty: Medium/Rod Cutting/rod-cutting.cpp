b569ca8dd434477bdc5453be924e8312c27b4a16
Adamya Kumar <101924838+Adamya-Kumar@users.noreply.github.com>
2025-10-08 02:06:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rod Cutting/rod-cutting.cpp
+++ Difficulty: Medium/Rod Cutting/rod-cutting.cpp
@@ -2,19 +2,16 @@
 
 class Solution {
   public:
-  
-  int rodCuttingUnboundedKnapsack(vector<int>& price,vector<int>& length,int n){
-      long long t[n+1][n+1];
+  int solve(vector<int>& price,vector<int>& length,int n){
+      int t[n+1][n+1];
       for(int i=0;i<n+1;i++){
-          for(int j=0;j<length.size()+1;j++){
-              if(i==0 || j == 0){
-                  t[i][j]=0;
-              }
+          for(int j=0;j<n+1;j++){
+              if(i==0||j==0)
+              t[i][j]=0;
           }
       }
-      
       for(int i=1;i<n+1;i++){
-          for(int j=1;j<length.size()+1;j++){
+          for(int j=1;j<n+1;j++){
               if(length[i-1]<=j){
                   t[i][j] = max(price[i-1]+t[i][j-length[i-1]],t[i-1][j]);
               }else{
@@ -22,14 +19,13 @@ class Solution {
               }
           }
       }
-      return t[n][length.size()];
+      return t[n][n];
   }
     int cutRod(vector<int> &price) {
         vector<int> length(price.size());
-        for(int i=0;i<price.size();i++){
-            length[i]=i+1;
+        for(int i=1;i<=price.size();i++){
+            length[i-1] =i;
         }
-       return rodCuttingUnboundedKnapsack(price,length,price.size());
-   
+        return solve(price,length,price.size());
     }
 };
